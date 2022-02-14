32238f5ad304698ae8ce7bfccac09944316c88b5
ECEAshwin <97493049+ECEAshwin@users.noreply.github.com>
2022-02-14 16:20:01 +0530

    Add files via upload

---
+++ Output Assignment 3.py
@@ -0,0 +1,65 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: D:\Ashwin\Python projects\Assignment 3.py ==============
+Question 1
+Enter your String : who can beat who
+who occurred 2 time
+can occurred 1 time
+beat occurred 1 time
+Question 2
+Day -6
+Month -5
+Year -1965
+Next Date is: 07/05/1965
+Question 3
+Enter number :5
+Add more number(Y/N) : Y
+Enter Number : 6
+Add more number(Y/N) : Y
+Enter Number : 3
+Add more number(Y/N) : N
+Output :  [(5, 25), (6, 36), (3, 9)]
+Question 4
+Enter Grade Point :7
+Your Grade is 'B' and Good.
+Question 5
+Enter String: ABCDEFGHIJK
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+Question 6
+Enter Name : ashwin
+Enter SID : 119
+Continue(Y/N) : Y
+Enter Name : piyush
+Enter SID : 200
+Continue(Y/N) : Y
+Enter Name : umesh
+Enter SID : 201
+Continue(Y/N) : N
+Student details :  {119: 'ashwin', 200: 'piyush', 201: 'umesh'}
+Sorted Names :  ['ashwin', 'piyush', 'umesh']
+Soarted SID :  [119, 200, 201]
+Enter SID : 119
+Name of student :  ashwin
+Question 7
+Enter the number of sequence : 8
+0
+1
+1
+2
+3
+5
+8
+13
+Average : 4.12
+Question 8
+a: {1, 3, 5, 6, 8}
+b: {17, 3, 6, 8, 9, 13}
+c: {1, 2, 4, 5}
+d: {6, 7, 8, 9, 10}
+e: {10, 7}
